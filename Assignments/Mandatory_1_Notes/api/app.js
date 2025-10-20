2b8f645ab2c21a7f17ad47dc4865b28180b4c91a
MadsLindersson <Mali0002@stud.kea.dk>
2025-10-20 14:28:02 +0200

    dqwd

--- Assignments/Mandatory_1_Notes/api/app.js
+++ Assignments/Mandatory_1_Notes/api/app.js
@@ -2,10 +2,21 @@
 import fs from 'fs';
 import express from 'express';
 import { marked } from "marked";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 
-const indexPage = fs.readFileSync('./public/pages/index/index.html', 'utf-8');
-const notePage = fs.readFileSync('./public/pages/notePage/notePage.html', 'utf-8');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const indexPage = fs.readFileSync(
+  path.join(__dirname, '../public/pages/index/index.html'),
+  'utf-8'
+);
+const notePage = fs.readFileSync(
+  path.join(__dirname, '../public/pages/notePage/notePage.html'),
+  'utf-8'
+);
 
 const app = express();
 app.use(express.static('public'));
@@ -15,7 +26,8 @@ app.get("/", (req, res) => {
 }); 
 
 app.get("/notes/:fileName", async (req, res) => {
-    const filePath = `./notes/${req.params.fileName}.md`;
+    const filePath = path.join(__dirname, '../notes', `${req.params.fileName}.md`);
+
 
     try {
         const mdFile = await fs.promises.readFile(filePath, 'utf-8');
